06479acbf4165ae65eff5e8351814cc07fbc0482
PyScientist <dsa.msk@gmail.com>
2023-04-15 19:29:05 +0700

    -Add some comments

--- tree_menu/templatetags/draw_menu.py
+++ tree_menu/templatetags/draw_menu.py
@@ -5,28 +5,26 @@ from tree_menu.models import Item
 
 register = template.Library()
 
-@register.simple_tag
-def current_time(format_string):
-    return datetime.datetime.now().strftime(format_string)
-
-
-@register.inclusion_tag("tree_menu/results.html")
-def show_results(name_of_list):
-    items = Item.objects.all()
-    return {"items": items, 'name': name_of_list}
-
 
 @register.inclusion_tag('tree_menu/nested_menu.html', takes_context=True)
-def draw_tree_menu(context, menu):
-
+def draw_tree_menu(context, menu: str) -> dict:
     try:
+        # !!!!Do request to the base and get items from given menu name!!!!!
         items = Item.objects.filter(menu__title=menu)
+        # Obtain list with dicts of param values for each item
         items_values = items.values()
+        # Obtain list with param dicts for items without parent
         primary_item = [item for item in items_values.filter(parent=None)]
+        # Return currently selected id's of items from context data (request from current menu)
         selected_item_id = int(context['request'].GET[menu])
+        # According to id has found
         selected_item = items.get(id=selected_item_id)
-        selected_item_id_list = get_selected_item_id_list(selected_item, primary_item, selected_item_id)
+        # Get list of id for selected item
+        selected_item_id_list = get_selected_item_id_list(parent=selected_item,
+                                                          primary_item=primary_item,
+                                                          selected_item_id=selected_item_id)
 
+        # For each item without parent
         for item in primary_item:
             if item['id'] in selected_item_id_list:
                 item['child_items'] = get_child_items(items_values, item['id'], selected_item_id_list)
@@ -40,6 +38,7 @@ def draw_tree_menu(context, menu):
             }
 
     result_dict['menu'] = menu
+    # prepare results for other menu if such exist
     result_dict['other_querystring'] = get_querystring(context, menu)
 
     return result_dict
@@ -47,7 +46,9 @@ def draw_tree_menu(context, menu):
 
 def get_querystring(context, menu):
     querystring_args = []
+    # get the dict with request header
     for key in context['request'].GET:
+        # if param header is not equal name of menu then form query string unit for this key
         if key != menu:
             querystring_args.append(f"{key}={context['request'].GET[key]}")
     querystring = ('&').join(querystring_args)
@@ -64,7 +65,7 @@ def get_child_items(items_values, current_item_id, selected_item_id_list):
 
 def get_selected_item_id_list(parent, primary_item, selected_item_id):
     selected_item_id_list = []
-
+    # While we don't get item without parent
     while parent:
         selected_item_id_list.append(parent.id)
         parent = parent.parent
